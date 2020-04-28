83e7ee55cae5f4b2e580aceafb91db29a1399e4b
John Neffenger <john@status6.com>
2020-04-28 09:52:38 -0700

    Upgrade to latest JDK and Maven plugin versions
    - Upgrade to the latest OpenJDK builds from Ubuntu, Oracle, and
      AdoptOpenJDK.
    - Compile the benchmarks with a target of JDK 11.
    - Upgrade to the latest Maven plugin versions.

--- bin/benchmarks.sh
+++ bin/benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Runs the benchmarks with the selected builds of OpenJDK
+# Runs the benchmarks with selected OpenJDK builds
 trap exit INT TERM
 set -o errexit
 
@@ -7,20 +7,19 @@ host=$(hostname --short)
 code=$(lsb_release --short --codename)
 arch=$(dpkg --print-architecture)
 date=$(date --iso-8601)
-jarfile=lib/benchmarks.jar
-
-# Oracle builds
-oracle13=$HOME/opt/jdk-13.0.2
+jarfile=target/benchmarks.jar
 
 # Ubuntu builds
 ubuntu11=/usr/lib/jvm/java-11-openjdk-$arch
-ubuntu13=/usr/lib/jvm/java-13-openjdk-$arch
-ubuntu14=/usr/lib/jvm/java-14-openjdk-$arch
+
+# Oracle builds
+oracle14=$HOME/opt/jdk-14.0.1
 
 # AdoptOpenJDK builds
-adopt11=$HOME/opt/jdk-11.0.6+10
+adopt11=$HOME/opt/jdk-11.0.7+10
 adopt12=$HOME/opt/jdk-12.0.2+10
 adopt13=$HOME/opt/jdk-13.0.2+8
+adopt14=$HOME/opt/jdk-14.0.1+7
 
 # Examples: -verbose:gc -Xlog:gc* -XX:+PrintCompilation
 jvmflags=''
@@ -31,11 +30,11 @@ filter=''
 # Examples: -f 1 -i 1 -wi 1 -p type=large
 jmhflags='-p type=large'
 
-# jdklist="$adopt11 $adopt12 $adopt13"
-jdklist="$ubuntu11 $ubuntu13 $ubuntu14"
-time for jdk in $jdklist; do
+jdklist="$adopt11 $adopt12 $adopt13 $adopt14"
+for jdk in $jdklist; do
     printf "\n[$(date)] Testing $jdk ...\n"
     $jdk/bin/java -version
     logfile=${host}-${code}-$(basename $jdk)-${date}.log
-    $jdk/bin/java $jvmflags -jar $jarfile $filter $jmhflags -o $logfile
+    time $jdk/bin/java $jvmflags -jar $jarfile $filter $jmhflags -o $logfile
+    grep "^Benchmarks" $logfile
 done
